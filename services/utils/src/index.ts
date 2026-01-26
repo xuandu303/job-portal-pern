b08679f9ea52aec5dfb890be324d869ac1154c5a
XuanDu-303 <phamxuandu13102003@gmail.com>
2026-01-27 00:15:10 +0700

    feat: set up utils service with Cloudinary

---
+++ services/utils/src/index.ts
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import express from "express";
+import routes from "./routes.js";
+import cors from "cors";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+const app = express();
+app.use(cors());
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.use("/api/utils", routes);
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Utils Service is running on http://localhost:${process.env.PORT}`,
+  );
+});
