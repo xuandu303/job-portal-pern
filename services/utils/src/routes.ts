61f758d2d7450cb509ae1f45a62ee6c358f23a82
XuanDu-303 <phamxuandu13102003@gmail.com>
2026-01-27 20:13:33 +0700

    feat: add cloudinary upload API

--- services/utils/src/routes.ts
+++ services/utils/src/routes.ts
@@ -1,5 +1,26 @@
 import express from "express";
+import cloudinary from "cloudinary";
 
 const router = express.Router();
+
+router.post("/upload", async (req, res) => {
+  try {
+    const { buffer, public_id } = req.body;
+    if (public_id) {
+      await cloudinary.v2.uploader.destroy(public_id);
+    }
+
+    const cloud = await cloudinary.v2.uploader.upload(buffer);
+
+    res.json({
+      url: cloud.secure_url,
+      public_id: cloud.public_id,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
 export default router;
