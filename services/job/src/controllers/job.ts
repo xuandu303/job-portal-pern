8f305d13fb28d1fd8dbae567dd0dd346e8875652
XuanDu-303 <phamxuandu13102003@gmail.com>
2026-02-07 07:01:59 +0700

    feat: add get company detail API

--- services/job/src/controllers/job.ts
+++ services/job/src/controllers/job.ts
@@ -118,3 +118,26 @@ export const updateJob = TryCatch(async (req: AuthenticatedRequest, res) => {
     job: updatedJob
   })
 })
+
+export const getAllCompany = TryCatch(async (req: AuthenticatedRequest, res) => {
+  const companies = await sql`SELECT * FROM companies WHERE recruiter_id = ${req.user?.user_id}`
+
+  res.json(companies)
+})
+
+export const getCompanyDetails = TryCatch(async (req: AuthenticatedRequest, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    throw new ErrorHandler(400, "Company id is required");
+  }
+
+  const [companyData] = await sql`SELECT c.*, COALESCE ((SELECT json_agg(j.*) FROM jobs j WHERE j.company_id = c.company_id), '[]'::json) AS jobs FROM companies c WHERE c.company_id = ${id}`;
+
+  if (!companyData) {
+    throw new ErrorHandler(404, "Company not found")
+  }
+
+  res.json(companyData)
+})
+
--- services/job/src/routes/job.ts
+++ services/job/src/routes/job.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { isAuth } from "../middlewares/auth.js";
 import uploadFile from "../middlewares/multer.js";
-import { createCompany, createJob, deleteCompany, updateJob } from "../controllers/job.js";
+import { createCompany, createJob, deleteCompany, getAllCompany, getCompanyDetails, updateJob } from "../controllers/job.js";
 
 const router = express.Router();
 
@@ -9,5 +9,7 @@ router.post("/company/new", isAuth, uploadFile, createCompany);
 router.delete("/company/:companyId", isAuth, deleteCompany);
 router.post("/new", isAuth, createJob);
 router.put("/:jobId", isAuth, updateJob);
+router.get("/company/all", isAuth, getAllCompany);
+router.get("/company/:id", isAuth, getCompanyDetails);
 
 export default router;
